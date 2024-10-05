4989d0cd3eab117b83561a72e844e8d3d9084fd5
Max Ohm <max.ohm@yale.edu>
2024-10-04 21:38:52 -0400

    Killed fireflykiller

--- Assets/Scripts/Player/FireflyMover.cs
+++ Assets/Scripts/Player/FireflyMover.cs
@@ -31,5 +31,14 @@ namespace Firefly
                 _turning = new();
             }
         }
+
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            if (col.gameObject.CompareTag("Obstacle"))
+            {
+                FireflyManager.Instance.OnFireFlyDied?.Invoke(transform.position);
+                Destroy(gameObject);
+            }
+        }
     }
 }
