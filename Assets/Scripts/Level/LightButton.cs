28ee95cca29e71df7f1bce741eff3f2dbd050e5a
Zhongye Wang <32293407+BruceZoom@users.noreply.github.com>
2024-10-07 11:45:19 -0400

    more visual effects

--- Assets/Scripts/Level/LightButton.cs
+++ Assets/Scripts/Level/LightButton.cs
@@ -1,7 +1,11 @@
 using System;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
 using MoreMountains.Feedbacks;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Rendering.Universal;
 
 namespace Firefly
 {
@@ -13,9 +17,15 @@ namespace Firefly
         public LightButtonEvent OnButtonChange = new LightButtonEvent();
         private Animator _anim;
 
+        [SerializeField] private float _targetIntensity;
+        private Light2D _light;
+        private TweenerCore<float, float, FloatOptions> _tween;
+
         private void Awake()
         {
             _anim = GetComponentInChildren<Animator>();
+            _light = GetComponent<Light2D>();
+            _light.intensity = 0f;
         }
 
         protected override void LightStateChange(LightState newState)
@@ -23,6 +33,12 @@ namespace Firefly
             OnButtonChange.Invoke(newState);
 
             _anim.SetBool("burn", newState == LightState.HasLight);
+
+            if (_tween != null && _tween.IsPlaying())
+            {
+                _tween.Kill();
+            }
+            _tween = DOTween.To(() => _light.intensity, v => _light.intensity = v, newState == LightState.HasLight ? _targetIntensity : 0, 1f);
         }
     }
 }
